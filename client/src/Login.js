55f23dc6c8cf05d9c2279166a60781fecb5944d1
crocoguyz <attheplay@gmail.com>
2026-04-17 18:10:11 +0630

    Login Page UI Design Update

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Lock, User, LogIn } from "lucide-react";
 import axios from "axios"; // axios ကို သုံးမယ်
 import "./login.css";
-import bgImage from "./assets/bg-kitchen.jpg";
+import bgImage from "./assets/bg-kitchen.png";
 
 export default function Login({ onLogin }) {
   const [credentials, setCredentials] = useState({ username: "", password: "" });
